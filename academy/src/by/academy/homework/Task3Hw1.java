2fc8c86a732fafa08e8c722422349496fab27f48
petuc <petuc@DESKTOP-K5A41G9>
2021-01-18 22:30:09 +0300

    fix bug with second incorrect input

--- academy/src/by/academy/homework/Task3Hw1.java
+++ academy/src/by/academy/homework/Task3Hw1.java
@@ -13,16 +13,15 @@ public class Task3Hw1 {
 		System.out.println("Введите число от 1 до 10 ");
 		a = console.nextInt();
 
-		if (a >= 0 && a <= 10) {
-
-			while (counter != 11) {
-				System.out.println(a * counter);
-				counter++;
-			}
-
-		} else {
+		while (a <= 0 || a >= 10) {
 			System.out.println("Число " + a + " не удовлетворяет условию. Введите число от 1 до 10");
+			a = console.nextInt();
 		}
+		while (counter != 11) {
+			System.out.println(a * counter);
+			counter++;
+		}
+
 		console.close();
 	}
 
