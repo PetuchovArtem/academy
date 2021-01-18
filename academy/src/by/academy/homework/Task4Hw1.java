c6910830ee1fc596b7649199cd4c70272d78177c
petuc <petuc@DESKTOP-K5A41G9>
2021-01-18 21:33:41 +0300

    add 4th task. not ready

---
+++ academy/src/by/academy/homework/Task4Hw1.java
@@ -0,0 +1,19 @@
+package by.academy.homework;
+
+public class Task4Hw1 {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int counter = 0;
+		int result = 0;
+
+		while (result < 1_000_000) {
+
+			result = (int) Math.pow(a, counter);
+			System.out.println(result);
+			counter++;
+		}
+
+	}
+
+}
