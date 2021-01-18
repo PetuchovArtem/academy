d3bb641a07f149dfcba59e1b65720d95596cd80e
petuc <petuc@DESKTOP-K5A41G9>
2021-01-18 20:07:50 +0300

    code optimize for task1

--- academy/src/by/academy/homework/Task1Hw1.java
+++ academy/src/by/academy/homework/Task1Hw1.java
@@ -2,42 +2,37 @@ package by.academy.homework;
 
 import java.util.Scanner;
 
-
 public class Task1Hw1 {
 
 	public static void main(String[] args) {
-		
+
 		int age;
 		double price;
 		double discount;
 		Scanner console = new Scanner(System.in);
-		
-		System.out.println ("Введите сумму покупки ");
+
+		System.out.println("Введите сумму покупки ");
 		price = console.nextDouble();
-		System.out.println ("Введите Ваш возраст ");
+		System.out.println("Введите Ваш возраст ");
 		age = console.nextInt();
-		
-		if(price<100) {
-			price=price*0.95;
-		}
-		else if(price>=100 && price<200) {
-			price=price*0.93;
-		}
-		else if(price>=200 && price<300) {
-			if (age>18)
-				price=price*0.84;
+
+		if (price < 100) {
+			price = price * 0.95;
+		} else if (price >= 100 && price < 200) {
+			price = price * 0.93;
+		} else if (price >= 200 && price < 300) {
+			if (age > 18)
+				price = price * 0.84;
 			else
-				price=price*0.91;	
+				price = price * 0.91;
+		} else if (price >= 300 && price < 400) {
+			price = price * 0.85;
+		} else if (price >= 400) {
+			price = price * 0.8;
 		}
-		else if(price>=300 && price<400) {
-			price=price*0.85;
-		}
-		else if(price>=400) {
-			price=price*0.8;
-		}
-		
-		System.out.println ("Финальная цена " + price);
-		
+
+		System.out.println("Финальная цена " + price);
+
 	}
 
 }
