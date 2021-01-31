c5759734f675554619b5fe2af55c70e648a5d16d
petuc <petuc@DESKTOP-K5A41G9>
2021-01-31 10:24:04 +0300

    changed structure git

--- academy/src/by/academy/homework/hw1/Task2Hw1.java
+++ academy/src/by/academy/homework/hw1/Task2Hw1.java
@@ -1,4 +1,4 @@
-package by.academy.homework;
+package by.academy.homework.hw1;
 
 import java.util.Scanner;
 
--- academy/src/by/academy/homework/hw1/Task4Hw1.java
+++ academy/src/by/academy/homework/hw1/Task4Hw1.java
@@ -1,4 +1,4 @@
-package by.academy.homework;
+package by.academy.homework.hw1;
 
 public class Task4Hw1 {
 
