b1af57cdcc3fe8ace9751883f97f35a5ae98f8de
petuc <petuc@DESKTOP-K5A41G9>
2021-03-21 12:31:50 +0300

    work with task2 Hw7

--- academy/src/by/academy/homework/hw7/task2/Main.java
+++ academy/src/by/academy/homework/hw7/task2/Main.java
@@ -1,5 +1,9 @@
 package by.academy.homework.hw7.task2;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -13,8 +17,36 @@ public class Main {
 		
 		System.out.println (ArtemUser.printUserInfo());
 		
+		Class<? extends User> userClass = User.class;
 		
+		//getDeclaredField("name")
+		try {
+			Field loginField = userClass.getDeclaredField("login");
+			String loginValue = (String) loginField.get(ArtemUser);
+			System.out.println(loginValue); 
+		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		//getDeclaredFields()
+		Field[] declaredFields = userClass.getDeclaredFields();
+
+		for (Field field : declaredFields) {
+			System.out.println(field);
+		}
+
 		
+				
 	}
 
 }
+
+//getMethod("name")
+//getMethods()
+//getField("name")
+//getFields()
+//а затем через 
+//getDeclaredMethod("name")
+//getDeclaredMethods()
+//getDeclaredField("name")
+//getDeclaredFields()
